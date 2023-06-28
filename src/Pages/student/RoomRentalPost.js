e247a83e8c979fe405a058fde3cf0ea7a4c81d96
wilsonyau02 <yauwilson02@gmail.com>
2023-06-28 16:32:50 +0800

    image layout

--- src/Pages/student/RoomRentalPost.js
+++ src/Pages/student/RoomRentalPost.js
@@ -36,12 +36,12 @@ function RoomRentalPost() {
         desktop: {
             breakpoint: { max: 3000, min: 1024 },
             items: 5,
-            slidesToSlide: 3 // optional, default to 1.
+            slidesToSlide: 1 // optional, default to 1.
         },
         tablet: {
             breakpoint: { max: 1024, min: 464 },
             items: 3,
-            slidesToSlide: 2 // optional, default to 1.
+            slidesToSlide: 1 // optional, default to 1.
         },
         mobile: {
             breakpoint: { max: 464, min: 0 },
@@ -253,7 +253,7 @@ function RoomRentalPost() {
 
             <div>
                 <Image.PreviewGroup>
-                    <Carousel responsive={responsive} itemClass=''>
+                    <Carousel responsive={responsive}>
                         {displayImages()}
                     </Carousel>
                 </Image.PreviewGroup>
