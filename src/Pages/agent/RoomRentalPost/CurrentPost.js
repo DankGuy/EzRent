0488865a4c40cdf248cd3c87183c5b8a435ffbbe
wilsonyau02 <yauwilson02@gmail.com>
2023-07-20 19:59:01 +0800

    status

--- src/Pages/agent/RoomRentalPost/CurrentPost.js
+++ src/Pages/agent/RoomRentalPost/CurrentPost.js
@@ -1,4 +1,4 @@
-import { Col, Image, Popconfirm, Popover, Row } from "antd";
+import { Col, Image, Popconfirm, Popover, Row, Tag } from "antd";
 import { FiEdit3 } from "react-icons/fi";
 import { GrView } from "react-icons/gr";
 import { MdOutlineDeleteOutline } from "react-icons/md";
@@ -7,6 +7,13 @@ import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { supabase } from "../../../supabase-client";
 import { getElapsedTime } from "../../../Components/timeUtils";
+import {
+    CheckCircleOutlined,
+    CloseCircleOutlined,
+    DollarOutlined,
+    SyncOutlined,
+  } from '@ant-design/icons';
+
 
 
 function CurrentPost({ post, deletePost, contextHolder }) {
@@ -39,14 +46,14 @@ function CurrentPost({ post, deletePost, contextHolder }) {
         padding: '0px 5px 0px 2px',
         fontSize: '16px',
         cursor: 'pointer',
-        margin: '5px 0px 5px 0px',    
+        margin: '5px 0px 5px 0px',
     };
 
     const content = (
-        <div 
+        <div
             style={{
-               width: '90px',
-               padding: '0px',
+                width: '90px',
+                padding: '0px',
             }}
         >
             <Row className="popOutBox">
@@ -83,7 +90,30 @@ function CurrentPost({ post, deletePost, contextHolder }) {
         </div>
     );
 
+    console.log(post);
 
+    const tagStatus = () => {
+
+        if (post.propertyStatus.stage === 'drafted') {
+            return ;
+        } else if (post.propertyStatus.stage === 'approved') {
+            return (
+                <Tag icon={<CheckCircleOutlined/>} color="success">Approved</Tag>
+            );
+        } else if (post.propertyStatus.stage === 'rejected') {
+            return (
+                <Tag icon={<CloseCircleOutlined/>} color="error">Rejected</Tag>
+            );
+        } else if (post.propertyStatus.stage === 'pending') {
+            return (
+                <Tag icon={<SyncOutlined spin />} color="processing">Pending</Tag>
+            );
+        } else if (post.propertyStatus.stage === 'rented') {
+            return (
+                <Tag icon={<DollarOutlined />} color="warning">Rented</Tag>
+            );
+        }
+    }
 
     return (
         <Col span={5} style={{ marginRight: '40px', marginBottom: '30px' }} key={post.postID}>
@@ -91,15 +121,19 @@ function CurrentPost({ post, deletePost, contextHolder }) {
                 style={{
                     height: 'auto',
                     marginTop: '10px',
-                    boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
-                    borderRadius: '5px',
-                    backgroundColor: 'white',
-                    border: '1px solid #e8e8e8',
-                    
+                    backgroundColor: '#f2f3f3', //fafafa f2f3f3
+                    borderCollapse: 'separate',
+                    borderSpacing: '0',
+                    boxSizing: 'border-box',
+                    boxShadow: 'var(--shadow-container-0bkp96,0 1px 1px 0 rgba(0,28,36,.3),1px 1px 1px 0 rgba(0,28,36,.15),-1px 1px 1px 0 rgba(0,28,36,.15))',
+                    fontFamily: 'var(--font-family-base-m6jzpk, "Amazon Ember", "Helvetica Neue", Roboto, Arial, sans-serif)',
                 }}
             >
                 <Row>
-                    <Col span={23} style={{ textAlign: 'right', margin: '5px 5px 5px 0px', fontStyle: 'italic', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
+                    <Col span={3} style={{ margin: '5px 0px 5px 5px' }}>
+                        {tagStatus()}
+                    </Col>
+                    <Col span={20} style={{ textAlign: 'right', margin: '5px 5px 5px 0px', fontStyle: 'italic', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
                         Last modified: {getElapsedTime(post.lastModifiedDate)}
                     </Col>
                 </Row>
@@ -109,7 +143,15 @@ function CurrentPost({ post, deletePost, contextHolder }) {
                     </Col>}
                 </Row>
                 <Row>
-                    <Col span={24} style={{ paddingLeft: '15px', fontSize: '18px', fontWeight: '500', paddingRight: '10px' }}>
+                    <Col span={24}
+                        style={{
+                            paddingLeft: '15px',
+                            fontSize: '18px',
+                            fontWeight: '700',
+                            letterSpacing: 'normal',
+                            lineHeight: '22px',
+                            paddingRight: '10px'
+                        }}>
                         {post.propertyName}
                     </Col>
                 </Row>
@@ -129,16 +171,19 @@ function CurrentPost({ post, deletePost, contextHolder }) {
                     </Col>
 
                     <Col span={2} offset={9}>
-                        <Popover 
-                            placement="leftTop" 
-                            arrow={false} 
-                            content={content} 
+                        <Popover
+                            placement="leftTop"
+                            arrow={false}
+                            style={{
+                                border: '1px solid blue',
+                            }}
+                            content={content}
                             trigger="click">
-                            <BsThreeDotsVertical size={18} 
-                                style={{ 
+                            <BsThreeDotsVertical size={18}
+                                style={{
                                     color: 'black',
                                     cursor: 'pointer',
-                                     }} />
+                                }} />
                         </Popover>
                     </Col>
                 </Row>
