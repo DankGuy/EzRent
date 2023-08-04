2f099c81fa27de514afa642506bba6cb881fb390
DankGuy <68477088+DankGuy@users.noreply.github.com>
2023-08-04 15:43:38 +0800

    admin

---
+++ src/Components/RentalAgreementDocument.js
@@ -0,0 +1,49 @@
+import React, { useRef } from "react";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
+import { Button } from 'antd';
+
+function RentalAgreementDocument({ componentToPrint }) {
+    const contentRef = useRef(null);
+
+    const exportPDF = async () => {
+        const content = contentRef.current;
+
+        // Use html2canvas to capture the content as an image with a higher resolution (scale 4)
+        const canvas = await html2canvas(content, {
+            scale: 4, // Adjust the scale to capture the content with higher resolution
+        });
+
+        // Convert the canvas image to a data URL
+        const imgData = canvas.toDataURL("image/jpeg", 1.0);
+
+        // Calculate the size of the PDF
+        const pdf = new jsPDF({
+            orientation: "portrait",
+            unit: "mm",
+            format: "a4",
+        });
+
+        // Calculate the aspect ratio to maintain content proportions
+        const imgWidth = pdf.internal.pageSize.getWidth();
+        const imgHeight = canvas.height * (imgWidth / canvas.width);
+
+        // Add the image to the PDF
+        pdf.addImage(imgData, "JPEG", 0, 0, imgWidth, imgHeight);
+
+        pdf.setFontSize(12);
+
+        // Save the PDF
+        pdf.save("rental_agreement.pdf");
+    };
+
+    return (
+        <div ref={contentRef}>
+            {/* Render your custom component inside the content div */}
+            {componentToPrint && componentToPrint()}
+            <Button onClick={exportPDF}>Generate PDF</Button>
+        </div>
+    );
+}
+
+export default RentalAgreementDocument;
--- src/Pages/agent/AgentRentalAgreement.js
+++ src/Pages/agent/AgentRentalAgreement.js
@@ -1,5 +1,94 @@
-function AgentRentalAgreement(){
-    return <div>Rental Agreement</div>
+import React from "react";
+import RentalAgreementDocument from "../../Components/RentalAgreementDocument";
+import EzRentIcon from "../../images/icon.png";
+import TarumtLogo from "../../images/tarumt.png";
+import { Descriptions } from 'antd';
+
+// Your custom component that you want to print to PDF
+function pdfContent() {
+  return (
+    <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+      <div className="header" style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "0 1rem",
+        marginBottom: "1rem",
+        flexDirection: "column",
+      }}>
+        <div className="logo" style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+          <img src={EzRentIcon} alt="logo" style={{ width: "60px", height: "60px", margin: "10px" }} />
+          <img src={TarumtLogo} alt="tarumt-logo" style={{ width: "100px", height: "60px", margin: "10px" }} />
+        </div>
+        <hr style={{ width: "100%", border: "1px solid black" }} />
+        <h1 style={{ textAlign: "center" }}>Rental Agreement</h1>
+
+        <div className="content-container" style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+          <div style={{ padding: "50px" }}>
+            <div>
+              <h2>Rental Details</h2>
+              <Descriptions>
+                <Descriptions.Item label="Referral Code" span={3}>KelvinEe-01-01</Descriptions.Item>
+                <Descriptions.Item label="Date" span={3}>2021-01-01</Descriptions.Item>
+                <Descriptions.Item label="Address" span={3}>12-34, PV12, No2A, Jalan Danau Saujana, Taman Danau Kota, 53300 Kuala Lumpur</Descriptions.Item>
+                <Descriptions.Item label="Commencement Date" span={2}>01/01/2023</Descriptions.Item>
+                <Descriptions.Item label="Expiry Date">01/01/2024</Descriptions.Item>
+                <Descriptions.Item label="Duration" span={3}>1 Year(s) 0 Month(s)</Descriptions.Item>
+                <Descriptions.Item label="Rental (/Month)">RM1098.00</Descriptions.Item>
+                <Descriptions.Item label="Security Deposit">RM1098.00</Descriptions.Item>
+                <Descriptions.Item label="Utilities Deposit">RM1098.00</Descriptions.Item>
+              </Descriptions>
+            </div>
+            <div className="terms" style={{ width: "100%" }}>
+              <h2>Terms and Conditions</h2>
+              <ol>
+                <li>Payment of rental shall be made on or before the 1st day of each month.</li>
+                <li>Payment of rental shall be made by cash or cheque or bank transfer to the Landlord's bank account.</li>
+                <li>Payment of rental shall be made by cash or cheque or bank transfer to the Landlord's bank account.</li>
+                <li>Payment of rental shall be made by cash or cheque or bank transfer to the Landlord's bank account.</li>
+                <li>Payment of rental shall be made by cash or cheque or bank transfer to the Landlord's bank account.</li>
+                <li>Payment of rental shall be made by cash or cheque or bank transfer to the Landlord's bank account.</li>
+                <li>Payment of rental shall be made by cash or cheque or bank transfer to the Landlord's bank account.</li>
+              </ol>
+            </div>
+
+            <div className="signature" style={{ width: "100%", display: "flex", flexDirection: "row" }}>
+              <div className="tenant-signature" style={{ marginRight: "470px" }}>
+                <h3>
+                  Signature by the tenant,
+                </h3>
+                <br />
+                <br />
+                <hr style={{ width: "20vw" }} />
+                <p><b>Name: </b></p>
+                <p><b>IC: </b></p>
+                <p><b>Date: </b></p>
+              </div>
+              <div className="agent-signature" style={{ float: "right" }}>
+                <h3>
+                  Signature by the agent,
+                </h3>
+                <br />
+                <br />
+                <hr style={{ width: "20vw" }} />
+                <p><b>Name: </b></p>
+                <p><b>IC: </b></p>
+                <p><b>Date: </b></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function AgentRentalAgreement() {
+  return (
+    <div>
+      <RentalAgreementDocument componentToPrint={pdfContent} />
+    </div>
+  );
 }
 
 export default AgentRentalAgreement;
